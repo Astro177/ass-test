024c771282f6c16f30ea7443a24f792ae7cef616
Astro <rgrxmahabub.official@gmail.com>
2023-03-03 15:31:25 +0600

    dodo

--- JS/index.js
+++ JS/index.js
@@ -1,19 +1,22 @@
 // fetching main api
+// let fetchSort = [];
 const fetchUiDetails = () => {
     URL = `https://openapi.programming-hero.com/api/ai/tools`
     fetch (URL)
     .then((res) => res.json())
-    .then((data) => showCardDetails(data.data));
+    .then((data) =>{ 
+      // fetchSort = data.data;
+      showCardDetails(data.data)});
 }
 // showing main details
 
-const showCardDetails = (data) =>{
+const showCardDetails = (details) =>{
     document.getElementById('spinner').classList.add('hidden');
       const cardContainer = document.getElementById('card-container');
-      data.tools.slice(0 , 6).forEach(singleCard => {
-        const { name, description , image , published_in , features , id } = singleCard;
+      details.tools.forEach(singleCard => {
+        const { name, image , published_in , features , id } = singleCard;
         const div = document.createElement("div");
-        div.classList.add("card", "w-full", "bg-base-100", "shadow-xl");
+        div.classList.add("card", "w-full", "bg-base-100", "shadow-2xl");
         div.innerHTML = `
         <figure class="px-10 pt-10">
                   <img src="${image}" alt="..." class="rounded-xl" />
@@ -44,7 +47,63 @@ const fetchBtnDetails = id =>{
     .then((data) => showBtnDetails(data.data));
 }
 const showBtnDetails = btnDetails =>{
-         
+ 
+  console.log(btnDetails);
+  const modalContainer = document.getElementById('modal-open');
+    const{ tool_name , description , image_link , input_output_examples , features , pricing, integrations , accuracy} = btnDetails;
+    const div = document.createElement("div");
+    div.classList.add("modal");
+    div.innerHTML = `
+    <div class="modal-box relative">
+            <label
+              for="my-modal-3"
+              class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>  
+             <div class="flex gap-5 p-5">
+             <div>
+              <div class="card w-full bg-base-100 shadow-xl">
+              <h3 class="text-lg font-bold px-5 pt-10">
+              ${description}
+            </h3>
+
+            <div class = "flex justify-between">
+            <div>
+            <p>${pricing[0].plan}<p>
+            <p>${pricing[0].price}<p>
+            </div>
+            <div>
+            <p>${pricing[1].plan}<p>
+            <p>${pricing[1].price}<p>
+            </div>
+            <div>
+            <p>${pricing[2].plan}<p>
+            <p>${pricing[2].price}<p>
+            </div>
+            <div>
+
+            </div>
+           </div>
+           <div>
+            <div class="card w-full bg-base-100 shadow-xl">
+  <figure class="px-10 pt-10">
+    <img src="${image_link[0]}" alt="..." class="rounded-xl">
+    <span class ="btn btn-error">${accuracy.score} %Accuracy</span>
+  </figure>
+  <div class="card-body items-center text-center">
+    <p class="text-xl font-bold">${input_output_examples[0].input}</p>
+    <p>${input_output_examples[0].output}</p>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+    `
+    modalContainer.appendChild(div);
+   ;
+  
+}
+
+const sortCard = () =>{
+  
 }
 
 showCardDetails();
